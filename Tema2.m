00ab6aa97451e2b119afca4f6dc227808eee6b8f
vaduvabianca <43204021+vaduvabianca@users.noreply.github.com>
2018-12-19 00:09:55 +0200

    Update Tema2.m

--- Tema2.m
+++ Tema2.m
@@ -1,57 +1,51 @@
-N = 50; % numarul de coeficienti
-D = 24; % durata
-P = 40; % perioada
-F=1/P; % frecventa 
-w0=2*pi/P; % pulsatia
-t=0:0.02:P-0.02; % timpul pe care calculam integrala (o perioada); 
-                 % rezolutia temporala trebuie sa fie de 2 ori mai mica decat perioada semnalului (teorema esantionarii);
-% semnalul dreptungiular
-x = zeros(1,size(t,2)); % initializarea lui x ( semnalul dreptunghiular) cu zerouri
-x(t<=D/2) =1; % de la 0 la D/2 x are valoarea 1 ( generam amplitundinea semnalului)
-x(t>P-D/2) =1; % de la D/2 la P x are valoarea 1 ( generam amplitundinea semnalului)
-t_4perioade = 0:0.02:4*P-0.02; % vectorul timp pentru reprezentarea pe 4 perioade
-x_4perioade = repmat(x,1,4); % vectorul x pentru reprezentarea pe 4 perioade ( genram 4 perioade de semnal dreptunghi)
-   
-% integrala numerica prin functia trapz
-for k = -N:N
-    x_temp = x;
-    x_temp = x_temp.*exp(-j*k*w0*t); % vectorul inmultit cu termenul corespunzator
-    X(k+51) = trapz(t,x_temp); % trapz calculeaza integrala prin metoda trapezului 
-    %(imparte suprafata in forme trapezoidale pentru a calcula mai usor aria)
-end
+%Semnal triunghiular
+%Perioada P = 40 s
+%Durata semnalelor D = 24
+%Rezolutie temporara adecvata
+%Numar de coeficienti = 50
 
-x_refacut(1:length(t)) = 0; % initializarea semnalului reconstruit cu N puncte
-
-%reconstructia lui x(t) folosind N coeficienti
-for index = 1:length(t);
-for k = -N:N
-x_refacut(index) = x_refacut(index) + (1/P)*X(k+N+1)*exp(j*k*w0*t(index));
-end
-end
-
+P = 40; 
+D = 24; 
+N = 50;
+w0 = 2*pi/P; %pulsatia unghiulara a semnalului
+t_tr = 0:0.02:D; %esantionarea semnalului original
+x_tr = sawtooth((pi/12)*t_tr,0.5)/2+0.5; %semnalul triunghiular original
+t = 0:0.02:P; %esantionarea semnalului modificat
+x = zeros(1,length(t)); %initializare a semnalului modificat cu valori nule
+x(t<=D) = x_tr; %modificam valorile nule cu valori din semnalul original
+                %unde avem valori din t <= durata semnalului 
 figure(1);
-plot(t_4perioade,x_4perioade); % afisarea lui x(t)
-title('x(t) cu linie solida si reconstructia folosind N=50 coeficienti(linie punctata)');
-hold on
-x_refacut_4perioade = repmat(x_refacut,1,4); % generarea lui x reconstruit pentru 4 perioade
-plot(t_4perioade,x_refacut_4perioade,'--'); % afisarea lui x reconstruit pentru 4 perioade
-xlabel('Timp [s]');
-ylabel('Amplitudine');
+plot(t,x),title('x(t)(linie solida) si reconstructia folosind N coeficienti (linie punctata)'); %afisare x(t)
+hold on;
 
-f = -N*F:F:N*F; % generarea vectorului de frecvente
+
+for k = -N:N %k este variabila dupa care se realizeaza suma
+    x_t = x_tr; %x_t e semnalul realizat dupa formula SF data
+    x_t = x_t .* exp(-1i*k*w0*t_tr); %inmultire intre doua matrice element cu element
+    X(k+N+1) = 0; %initializare cu valoare nula
+    for i = 1:length(t_tr)-1
+        X(k+N+1) = X(k+N+1) + (t_tr(i+1)-t_tr(i)) * (x_t(i)+x_t(i+1))/2; %reconstructia folosind coeficientii
+    end
+end
+
+for i = 1:length(t) %i este variabila dupa care se realizeaza suma
+    x_finit(i) = 0; %initializare cu valoare nula
+    for k=-N:N
+        x_finit(i) = x_finit(i) + (1/P) * X(k+51) * exp(1i*k*w0*t(i));  %reconstructia folosind coeficientii
+    end
+end
+plot(t,x_finit,'--'); %afisare reconstructie semnal folosind cei N coeficienti
+
 figure(2);
-stem(f,abs(X)); % afisarea lui X
-title('Spectrul lui x(t)');
-xlabel('Frecventa [Hz]');
-ylabel('|X|');
+w=-50*w0:w0:50*w0; %w este vectorul ce ne va permite afisarea spectrului functiei
+stem(w/(2*pi),abs(X)),title('Spectrul lui x(t)'); %afisarea spectrului
 
-%
-% Seriile Fourier  ne spunca orice semnal periodic poate fi aproximat
-% printr-o suma infinita de sin si cos.
-% Coeficienti lui si sin si cos reprezinta spectrul de amplitudini al
-% semnalului.
-% Semnalul reconstruit folosind un numar finit de termeni se apropie ca
-% forma de semnalul original dar are o anume eroare. Cu cat marim
-% numarul de coeficienti cu atat aceasta  eroare va fi  mai mica. 
-%In plus se observa faptul ca semnalul poate fi aproximat printr-o
-% suma de sinusoide: variatiile semanlului au un caracter de sinusoida.
+%Conform cursului de SS, teoria seriilor Fourier (trigonometrica, armonica si complexa) ne spune
+%ca orice semnal periodic poate fi aproximat printr-o suma infinita de sinusuri
+%si cosinusuri de diferite frecvente, fiecare ponderat cu un anumit coeficient. 
+%Acesti coeficienti reprezinta practic spectrul (amplitudinea pentru anumite frecvente).
+%Semnalul reconstruit foloseste un numar finit de termeni(N=50 in tema) si se apropie ca
+%forma de semnalul original, insa prezinta o marja de eroare. Cu cat marim
+%numarul de coeficienti ai SF, aceasta marja de eroare va fi din ce in ce mai
+%mica. In plus se observa faptul ca semnalul poate fi aproximat printr-o
+%suma de sinusoide, variatiile semnalului prezentand un caracter sinusoidal.
